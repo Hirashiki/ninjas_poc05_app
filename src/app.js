9a28a37ec2e75daeef260d0b094876625e64804f
Ricardo hirashiki <ricardo.hirashiki@portoseguro.com.br>
2017-10-19 21:04:47 -0200

    cors

--- src/app.js
+++ src/app.js
@@ -4,5 +4,10 @@ var path = require('path');
 
 //setting middleware
 app.use(express.static(__dirname )); //Serves resources from public folder
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 app.listen(8080);
