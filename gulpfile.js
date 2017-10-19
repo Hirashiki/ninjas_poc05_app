92ca08ba390f3aa0df485ace5668a616118a423d
Ricardo hirashiki <ricardo.hirashiki@portoseguro.com.br>
2017-10-19 19:27:41 -0200

    teste chamada api

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 //http://rcdevlabs.github.io/2015/02/17/googlemaps-api-1-3-criando-um-mapa-simples/
 var gulp = require('gulp')
 , connect = require('gulp-connect-multi')();
+var cors1 = require('cors');
+
+var cors = function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  next();
+};
+
   
 gulp.task('arquivos', function(){
  gulp.src(['./src/**/**/*.*'])
@@ -14,6 +22,9 @@ gulp.task('servidor', connect.server({
  	livereload: true,
  	open: {
     browser: 'chrome' //ou firefox ou safari... se for OSX é 'Google Chrome'
+  },
+  middleware: function() {
+    return [cors];
   }
 }))
 
